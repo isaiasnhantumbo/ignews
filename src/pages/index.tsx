b68f4f35a9e6e73e4bde4c9bc496925751c18168
Isaias Nhantumbo <isaiasnhantumbo54@gmail.com>
2021-07-28 17:19:26 +0200

    :tada: Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Inicio | ig.news</title>
+      </Head>
+      <h1>
+        Hello <span>World</span>
+      </h1>
+    </>
+  );
+}
