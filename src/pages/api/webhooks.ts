98b0d0894c6169405895af43d428012646cf9654
Isaias Nhantumbo <isaiasnhantumbo54@gmail.com>
2021-08-03 18:12:29 +0200

    :tada: feat: added buffer function

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -1,5 +1,16 @@
 /* eslint-disable import/no-anonymous-default-export */
 import { NextApiRequest, NextApiResponse } from 'next';
+import { Readable } from 'stream';
+
+async function buffer(readable: Readable) {
+  const chunks = [];
+
+  for await (const chunk of readable) {
+    chunks.push(typeof chunk === 'string' ? Buffer.from(chunk) : chunk);
+  }
+
+  return Buffer.concat(chunks);
+}
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
   console.log('evento recebido');
