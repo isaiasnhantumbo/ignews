619527953a4c41557bb0f25835ef5f18872d7975
Isaias Nhantumbo <isaiasnhantumbo54@gmail.com>
2021-08-04 13:41:59 +0200

    :key: added PRISMIC_ACESS_TOKEN

--- src/services/prismic.ts
+++ src/services/prismic.ts
@@ -1,7 +1,8 @@
 import Prismic from '@prismicio/client';
 
-export function getPrismicClient() {
-  const prismic = Prismic.client(process.env.PRISMIC_ACCESS_TOKEN, {
+export function getPrismicClient(req?: unknown) {
+  const prismic = Prismic.client(process.env.PRISMIC_ENDPOINT, {
+    req,
     accessToken: process.env.PRISMIC_ACCESS_TOKEN,
   });
 
