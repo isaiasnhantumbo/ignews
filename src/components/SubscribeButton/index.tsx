3fa8c863bc4618aa11cbd16809f84b3b6f034fcf
Isaias Nhantumbo <isaiasnhantumbo54@gmail.com>
2021-08-04 13:44:11 +0200

    :tada: feat: check if user have subscription

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,6 @@
 import { signIn, useSession } from 'next-auth/client';
+import { useRouter } from 'next/router';
+
 import { api } from '../../services/api';
 import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
@@ -8,12 +10,19 @@ interface SubscribeButtonProps {
 }
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
   const [session] = useSession();
+  const router = useRouter();
 
   async function handleSubscribe() {
     if (!session) {
       signIn('github');
       return;
     }
+
+    if (session?.activeSubscription) {
+      router.push('/posts');
+      return;
+    }
+
     try {
       const response = await api.post('/subscribe');
 
